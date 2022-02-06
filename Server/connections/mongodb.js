f333b9f51199a05699f6ae4b3ef5ba4f0f1bc7a4
Chris Jacob <chrisjacob876@gmail.com>
2022-02-06 12:06:10 +0530

    final

---
+++ Server/connections/mongodb.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+module.exports = async () => {
+    try {
+        const connectionParams = {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+        };
+        await mongoose.connect('mongodb://localhost:27017/myapp');
+        console.log("Connected to database.");
+    } catch (error) {
+        console.log("Could not connect to database.", error);
+    }
+};
